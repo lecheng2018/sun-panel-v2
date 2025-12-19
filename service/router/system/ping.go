760e17788c02cb746523137c537910dd6f23aeca
高学宇 <75412701@qq.com>
2025-12-19 19:42:30 +0800

    ping接口去除同源策略

--- service/router/system/ping.go
+++ service/router/system/ping.go
@@ -9,6 +9,18 @@ import (
 func InitPingRouter(router *gin.RouterGroup) {
 	ping := api_v1.ApiGroupApp.ApiSystem.Ping
 	{
-		router.GET("ping", ping.Get)
+		router.GET("ping", func(c *gin.Context) {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "*")
+			c.Next()
+		}, ping.Get)
+
+		router.OPTIONS("ping", func(c *gin.Context) {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "*")
+			c.Status(204)
+		})
 	}
 }
