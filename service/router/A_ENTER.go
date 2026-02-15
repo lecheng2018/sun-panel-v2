e289c27bcdfe3673618808d93e08a74c34170fc4
高学宇 <75412701@qq.com>
2026-02-16 01:48:23 +0800

    修复外网访问

--- service/router/A_ENTER.go
+++ service/router/A_ENTER.go
@@ -48,7 +48,7 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", "*") // Or origin
+			c.Header("Access-Control-Allow-Origin", origin) // Set specific origin instead of '*'
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, token, x-token")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
@@ -59,6 +59,7 @@ func Cors() gin.HandlerFunc {
 
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
+			return // Ensure we stop here for preflights
 		}
 
 		c.Next()
