beff0d0cd86f9d1f1013fd6e51cc296ef90a4d45
高学宇 <75412701@qq.com>
2026-02-16 02:44:19 +0800

    修复外网访问

--- service/router/A_ENTER.go
+++ service/router/A_ENTER.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"net/http"
 	"sun-panel/global"
+
 	// "sun-panel/router/admin"
 	"sun-panel/router/openness"
 	"sun-panel/router/panel"
@@ -15,6 +17,7 @@ func InitRouters(addr string) error {
 	router := gin.Default()
 	rootRouter := router.Group("/")
 	routerGroup := rootRouter.Group("api")
+	routerGroup.Use(Cors()) // Apply CORS to all /api routes
 
 	// 接口
 	system.Init(routerGroup)
@@ -38,4 +41,27 @@ func InitRouters(addr string) error {
 
 	global.Logger.Info("Sun-Panel is Started.  Listening and serving HTTP on ", addr)
 	return router.Run(addr)
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, token, x-token")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+			// Private Network Access support
+			c.Header("Access-Control-Allow-Private-Network", "true")
+		}
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
 }
