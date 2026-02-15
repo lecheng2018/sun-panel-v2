7f4cc8b28f5b2351b2a859c307efcdc02ec76eea
高学宇 <75412701@qq.com>
2026-02-16 02:18:37 +0800

    修复外网访问

--- service/router/A_ENTER.go
+++ service/router/A_ENTER.go
@@ -7,15 +7,12 @@ import (
 	"sun-panel/router/panel"
 	"sun-panel/router/system"
 
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 // 初始化总路由
 func InitRouters(addr string) error {
 	router := gin.Default()
-	router.Use(Cors())
 	rootRouter := router.Group("/")
 	routerGroup := rootRouter.Group("api")
 
@@ -41,27 +38,4 @@ func InitRouters(addr string) error {
 
 	global.Logger.Info("Sun-Panel is Started.  Listening and serving HTTP on ", addr)
 	return router.Run(addr)
-}
-
-func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", origin) // Set specific origin instead of '*'
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token, token, x-token")
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-			c.Header("Access-Control-Allow-Credentials", "true")
-			// Private Network Access support
-			c.Header("Access-Control-Allow-Private-Network", "true")
-		}
-
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return // Ensure we stop here for preflights
-		}
-
-		c.Next()
-	}
 }
--- service/router/system/ping.go
+++ service/router/system/ping.go
@@ -1,14 +1,33 @@
 package system
 
 import (
+	"net/http"
 	"sun-panel/api/api_v1"
 
 	"github.com/gin-gonic/gin"
 )
 
+func PingCors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		c.Header("Access-Control-Allow-Private-Network", "true")
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func InitPingRouter(router *gin.RouterGroup) {
 	ping := api_v1.ApiGroupApp.ApiSystem.Ping
-	{
-		router.GET("ping", ping.Get)
-	}
+	router.GET("ping", PingCors(), ping.Get)
+	router.OPTIONS("ping", PingCors(), func(c *gin.Context) {
+		c.AbortWithStatus(http.StatusNoContent)
+	})
 }
